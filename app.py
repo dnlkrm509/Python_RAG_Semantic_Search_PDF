7dcec4527d26ae45c6ecc25fb6d150dddaf1605b
Daniel Karimi <109435295+dnlkrm509@users.noreply.github.com>
2026-05-15 01:25:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -226,5 +226,5 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)
+        port=int(os.environ.get("PORT", 10000))
     )
